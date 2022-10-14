1a0aace711a17a1cb42028b39db5bfdfd3dda3dc
parth chaturvedi <parthbhai012@gmail.com>
2022-10-14 19:55:23 +0530

    adding scripts for heroku

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors=require("cors");
+require("dotenv").config();
 const app=express();
 const PORT = process.env.PORT || 8080;
 const host='0.0.0.0';
@@ -7,17 +9,23 @@ const path = require("path");
 require("./db/conn");
 const exphbs = require("express-handlebars");
 const bodyparser =require("body-parser");
-
+app.use(cors()); 
 //app.engine('handlebars',exphbs({extname:"hbs",defaultLayout:false,layoutDir:"views/"}));
 app.set('veiw engine','handlebars');
 
 
 
+//calling
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
+app.get("/get-auth-code", (req, res, next) => {
+    return res.send(
+      `<a href='https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_APP_ID}&redirect_uri=${process.env.REDIRECT_URI}&scope=user_media,user_profile&response_type=code'> Connect to Instagram </a>`
+    );
+  });
 const routes = require("./contoller/user")
 app.use('/', routes)
 app.listen(PORT,host,()=>
