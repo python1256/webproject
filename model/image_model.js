f76749793fc6a886ccfc633e563ac8d78debd912
parth chaturvedi <parthbhai012@gmail.com>
2022-10-18 17:29:17 +0530

    adding to heroku

---
+++ model/image_model.js
@@ -0,0 +1,12 @@
+const mongoose=request("mongoose");
+const Imageschema = new mongoose.Schema({
+   name:String,
+   desc:String,
+   img:{
+    data:Buffer,
+    contentType:String
+   }
+});
+const Image_store=new mongoose.model('Image_store',Imageschema);
+
+module.exports=Image_store;
