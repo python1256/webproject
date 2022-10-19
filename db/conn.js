5e6c2b1abd26543cc0d4b203db07dbfcdef3639b
parth chaturvedi <parthbhai012@gmail.com>
2022-10-19 18:35:12 +0530

    adding to heroku

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://influencer-db:d1oLzPnOirNkuglYIOjgA9pzt1jr5reFGVMpMWJTd4QlyHUr32kGxUwkSJLuBoJDOoJfXJMwvUpuGHFZCNmcmg==@influencer-db.mongo.cosmos.azure.com:10255/paru_db?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@influencer-db@").then((db)=>{
+mongoose.connect("mongodb://tll-db-new:MhgZPAIxcc2m61bSK3DXW7KSghAaqSOotdiHlVQuRzi5bQ9QYWhM1yVS1BGpvQT6loFq2J4SNCU6F1cScbXqOw==@tll-db-new.mongo.cosmos.azure.com:10255/paru_db?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@tll-db-new@").then((db)=>{
     console.log("connection is succesful");
 }).catch((err)=>{
     console.log("no connection",err);
