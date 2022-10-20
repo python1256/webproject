b1c0029af3bebc675e6acb86d8047cc9cf8dc5ec
parth chaturvedi <parthbhai012@gmail.com>
2022-10-21 00:07:33 +0530

    adding to heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors=require("cors");
 const app=express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 const host='0.0.0.0';
 const nodemailer = require("nodemailer");
 const path = require("path");
