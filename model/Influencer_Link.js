b31c9b0b023b6dcb5ff53dfd1609c516508af695
parth chaturvedi <parthbhai012@gmail.com>
2022-11-18 15:29:09 +0530

    adding to heroku

--- model/Influencer_Link.js
+++ model/Influencer_Link.js
@@ -3,57 +3,27 @@ const mongoose=require("mongoose");
 const Linkschema = new mongoose.Schema({
     Instagram_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],       
-        required:[
-            true,"link filed is mandatory"
-        ],
+        
     },
     Facebook_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],
-        required:[
-            true,"link filed is mandatory"
-        ],
+        
     },
     tiktok_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],
-        required:[
-            true,"link filed is mandatory"
-        ],
+        
     },
     Youtube_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],
-        required:[
-            true,"link filed is mandatory"
-        ],
+        
     },
     Twitter_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],
-        required:[
-            true,"link filed is mandatory"
-        ],
+        
     },
     website_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],
-        required:[
-            true,"link filed is mandatory"
-        ],
+        
     }
 })
 const InfluencerLink_store=new mongoose.model('InfluencerLink_store',Linkschema);
