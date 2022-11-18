2a9c1829d65c8dc1ce3d8a5203bf8921cf14bbff
parth chaturvedi <parthbhai012@gmail.com>
2022-11-18 15:21:50 +0530

    adding to heroku

--- model/Brands_Link.js
+++ model/Brands_Link.js
@@ -3,57 +3,28 @@ const mongoose=require("mongoose");
 const Linkschema = new mongoose.Schema({
     Instagram_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],
-        required:[
-            true,"email filed is mandatory"
-        ],
-    },
+        
+        },
     Facebook_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],
-        required:[
-            true,"link filed is mandatory"
-        ],
+        
+        
     },
     tiktok_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],
-        required:[
-            true,"link filed is mandatory"
-        ],
+        
     },
     Youtube_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],
-        required:[
-            true,"link filed is mandatory"
-        ],
+        
     },
     Twitter_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],
-        required:[
-            true,"email filed is mandatory"
-        ],
+        
     },
     website_Link:{
         type:String,
-        unique:[
-            true,"link is mandatory"
-        ],
-        required:[
-            true,"link filed is mandatory"
-        ],
+        
     }
 })
 const BrandsLink_store=new mongoose.model('BrandsLink_store',Linkschema);
